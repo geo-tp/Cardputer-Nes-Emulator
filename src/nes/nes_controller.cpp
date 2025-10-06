aec616b8d895a27aae593ab5065c43157e91a836
Geo <tp53.geo@gmail.com>
2025-10-06 14:54:36 +0200

    allow more control buttons for brightness and sound

--- src/nes/nes_controller.cpp
+++ src/nes/nes_controller.cpp
@@ -7,11 +7,33 @@ void controller_init() {
 }
 
 uint32_t controller_read_input() {
-  uint32_t value = 0xFFFFFFFF;
-  auto &kb = M5Cardputer.Keyboard;
+    uint32_t value = 0xFFFFFFFF;
 
-  M5Cardputer.update();
-        
+    M5Cardputer.update();
+    Keyboard_Class::KeysState status = M5Cardputer.Keyboard.keysState();
+
+    // Volume up
+    if (M5Cardputer.Keyboard.isKeyPressed('=') || (status.fn && M5Cardputer.Keyboard.isKeyPressed(';'))) {
+        M5Cardputer.Speaker.setVolume(min(M5Cardputer.Speaker.getVolume() + 5, 200)); // volume up
+        return value;
+    }
+
+    // Volume down
+    if (M5Cardputer.Keyboard.isKeyPressed('-') || (status.fn && M5Cardputer.Keyboard.isKeyPressed('.'))) {
+        M5Cardputer.Speaker.setVolume(max(M5Cardputer.Speaker.getVolume() - 5, 0)); // volume down
+        return value;
+    }
+
+    // Brightness control
+    if (M5Cardputer.Keyboard.isKeyPressed(']') || (status.fn && M5Cardputer.Keyboard.isKeyPressed('/'))) {
+        M5Cardputer.Display.setBrightness(min(M5Cardputer.Display.getBrightness() + 10, 255)); // brightness up
+        return value;
+    }
+    if (M5Cardputer.Keyboard.isKeyPressed('[') || (status.fn && M5Cardputer.Keyboard.isKeyPressed(','))) {
+        M5Cardputer.Display.setBrightness(max(M5Cardputer.Display.getBrightness() - 10, 0)); // brightness down
+        return value;
+    }
+ 
     // Arrows and buttons
     if (M5Cardputer.Keyboard.isKeyPressed('a') || M5Cardputer.Keyboard.isKeyPressed(',')) {
         value ^= (1 << 2); // left
@@ -42,21 +64,7 @@ uint32_t controller_read_input() {
         value ^= (1 << 7); // B
     }
 
-    // Sound and brightness control
-    if (M5Cardputer.Keyboard.isKeyPressed('=')) {
-        M5Cardputer.Speaker.setVolume(min(M5Cardputer.Speaker.getVolume() + 5, 200)); // volume up
-    }
-    if (M5Cardputer.Keyboard.isKeyPressed('-')) {
-        M5Cardputer.Speaker.setVolume(max(M5Cardputer.Speaker.getVolume() - 5, 0)); // volume down
-    }
-    if (M5Cardputer.Keyboard.isKeyPressed(']')) {
-        M5Cardputer.Display.setBrightness(min(M5Cardputer.Display.getBrightness() + 10, 255)); // brightness up
-    }
-    if (M5Cardputer.Keyboard.isKeyPressed('[')) {
-        M5Cardputer.Display.setBrightness(max(M5Cardputer.Display.getBrightness() - 10, 0)); // brightness down
-    }
-
-  return value;
+    return value;
 }
 
 } // extern "C"
