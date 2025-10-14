430b5318294569e6de93aefeb4f190c56ee91623
Geo <tp53.geo@gmail.com>
2025-10-14 14:27:47 +0200

    add confirmation selector for resume last game

---
+++ src/selector/ConfirmationSelector.h
@@ -0,0 +1,19 @@
+#ifndef CONFIRMATION_SELECTOR_H
+#define CONFIRMATION_SELECTOR_H
+
+#include <string>
+#include <Arduino.h>
+#include "input/CardputerInput.h"
+#include "display/CardputerView.h"
+
+class ConfirmationSelector {
+public:
+    ConfirmationSelector(CardputerView& display, CardputerInput& input);
+    bool select(const std::string& title, const std::string& description);
+
+private:
+    CardputerView& display;
+    CardputerInput& input;
+};
+
+#endif // CONFIRMATION_SELECTOR_H
