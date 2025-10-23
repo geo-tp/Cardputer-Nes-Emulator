0b6d3828ddc2fd394fb2f6021e3e8f6a93669aa2
Geo <tp53.geo@gmail.com>
2025-10-23 07:57:54 +0200

    allocate buffer dynamically for nes render

--- src/nes/nes_display.cpp
+++ src/nes/nes_display.cpp
@@ -5,6 +5,8 @@ extern "C" {
 #include <M5Unified.h>
 
 int16_t frame_scaling;
+static uint16_t* s_nesLineBuf = nullptr;
+static int       s_nesLineBufLen = 0;  // en pixels
 static int16_t frame_x, frame_y, frame_x_offset, frame_y_offset, frame_width, frame_height, frame_line_pixels;
 extern uint16_t myPalette[];
 extern bool fullscreenMode;
@@ -21,58 +23,65 @@ void display_init() {
     frame_y = 0;
     frame_x_offset = 0;
     frame_y_offset = 90;
-    frame_width = M5.Lcd.width();
-    frame_height = M5.Lcd.height();
+
+    frame_width  = M5.Lcd.width();   // typ 240
+    frame_height = M5.Lcd.height();  // typ 135
     frame_line_pixels = frame_width;
+
+    if (!s_nesLineBuf || s_nesLineBufLen != frame_width) {
+        if (s_nesLineBuf) {
+            heap_caps_free(s_nesLineBuf);
+            s_nesLineBuf = nullptr;
+            s_nesLineBufLen = 0;
+        }
+
+        size_t bytes = frame_width * sizeof(uint16_t);
+        s_nesLineBuf = (uint16_t*)heap_caps_malloc(bytes, MALLOC_CAP_INTERNAL | MALLOC_CAP_8BIT);
+        if (!s_nesLineBuf) s_nesLineBuf = (uint16_t*)malloc(bytes); // fallback
+
+
+        if (s_nesLineBuf) {
+            memset(s_nesLineBuf, 0, bytes);
+            s_nesLineBufLen = frame_width;
+        }
+    }
 }
 
 void display_write_frame_square(const uint8_t *data[]) {
     M5.Lcd.startWrite();
     M5.Lcd.setWindow(frame_x, frame_y, frame_width - 1, frame_height - 1);
 
-    const int srcW = NES_SCREEN_WIDTH;
-    const int srcH = NES_SCREEN_HEIGHT;
+    const int srcW = NES_SCREEN_WIDTH;   // 256
+    const int srcH = NES_SCREEN_HEIGHT;  // 240
 
     int outH = frame_height;
     int desiredW = 220; 
-
     int outW = desiredW;
     if (outW > frame_width) outW = frame_width;
 
     const int x_start = (frame_width - outW) / 2;
-    const int x_end   = x_start + outW;          // exclusive
+    const int x_end   = x_start + outW;
     const int virtW   = (srcH * 4) / 3;
     const int virtOff = (virtW - srcW) / 2;
 
-    static uint16_t lineBuf[NES_SCREEN_WIDTH];
-
     for (int32_t y = 0; y < frame_height; ++y) {
-        // Map Y screen -> Y NES 
         int srcY = (y * srcH) / outH;
         if (srcY < 0) srcY = 0; else if (srcY >= srcH) srcY = srcH - 1;
 
-        // Black bars on the left
         for (int32_t x = 0; x < x_start; ++x) {
-            lineBuf[x] = TFT_BLACK;
+            s_nesLineBuf[x] = TFT_BLACK;
         }
-
-        // Active area
         for (int32_t x = x_start; x < x_end; ++x) {
-            // virtX dans 0..virtW-1, proportional to x in x_start..x_end-1
             int virtX = ((x - x_start) * virtW) / outW;
-            int srcX  = virtX - virtOff; // center
-            lineBuf[x] = (unsigned)srcX < (unsigned)srcW
+            int srcX  = virtX - virtOff;
+            s_nesLineBuf[x] = (unsigned)srcX < (unsigned)srcW
                 ? myPalette[data[srcY][srcX]]
-                : TFT_BLACK; // safety
+                : TFT_BLACK;
         }
-
-        // Black bars on the right
         for (int32_t x = x_end; x < frame_width; ++x) {
-            lineBuf[x] = TFT_BLACK;
+            s_nesLineBuf[x] = TFT_BLACK;
         }
-
-        // Push a complete line
-        M5.Lcd.pushPixels(lineBuf, frame_width);
+        M5.Lcd.pushPixels(s_nesLineBuf, frame_width);
     }
 
     M5.Lcd.endWrite();
@@ -81,15 +90,12 @@ void display_write_frame_square(const uint8_t *data[]) {
 static inline int clampi(int v, int lo, int hi){ return v < lo ? lo : (v > hi ? hi : v); }
 
 void display_write_frame_zoom(const uint8_t *data[]) {
-    static uint16_t lineBuf[NES_SCREEN_WIDTH];
-
     M5.Lcd.startWrite();
     M5.Lcd.setWindow(frame_x, frame_y, frame_width - 1, frame_height - 1);
 
     const int srcW = NES_SCREEN_WIDTH;   // 256
     const int srcH = NES_SCREEN_HEIGHT;  // 240
 
-    // Center
     uint32_t zp = (nesZoomPercent < 100) ? 100u : (uint32_t)nesZoomPercent;
     int roiW = (int)((uint32_t)srcW * 100u / zp);
     int roiH = (int)((uint32_t)srcH * 100u / zp);
@@ -103,13 +109,20 @@ void display_write_frame_zoom(const uint8_t *data[]) {
         int srcY = roiY0 + (int)((int32_t)y * roiH / frame_height);
         srcY = clampi(srcY, 0, srcH - 1);
 
+        // bord gauche
+        s_nesLineBuf[0] = s_nesLineBuf[1] = s_nesLineBuf[2] = s_nesLineBuf[3] = BLACK;
+
         for (int32_t x = 4; x < frame_width - 1; x++) {
             int srcX = roiX0 + (int)((int32_t)(x - 4) * roiW / (frame_width - 5));
             srcX = clampi(srcX, 0, srcW - 1);
-            lineBuf[x] = myPalette[data[srcY][srcX]];
+            s_nesLineBuf[x] = myPalette[data[srcY][srcX]];
         }
 
-        M5.Lcd.pushPixels(lineBuf, frame_width);
+        // bord droit
+        s_nesLineBuf[frame_width - 2] = BLACK;
+        s_nesLineBuf[frame_width - 1] = BLACK;
+
+        M5.Lcd.pushPixels(s_nesLineBuf, frame_width);
     }
 
     M5.Lcd.endWrite();
@@ -120,33 +133,28 @@ void display_write_frame(const uint8_t *data[]) {
         display_write_frame_square(data);
         return;
     }
-
     if (nesZoomPercent != 100) {
         display_write_frame_zoom(data);
         return;
     }
 
-    static uint16_t lineBuf[NES_SCREEN_WIDTH];
-
     M5.Lcd.startWrite();
     M5.Lcd.setWindow(frame_x, frame_y, frame_width - 1, frame_height - 1);
 
     for (int32_t y = 0; y < frame_height; y++) {
-        int srcY = (y * NES_SCREEN_HEIGHT) / frame_height; 
+        int srcY = (y * NES_SCREEN_HEIGHT) / frame_height;
 
-        // hack to avoid frame bleeding
-        lineBuf[0] = BLACK;
-        lineBuf[1] = BLACK;
-        lineBuf[2] = BLACK;
-        lineBuf[3] = BLACK;
+        s_nesLineBuf[0] = s_nesLineBuf[1] = s_nesLineBuf[2] = s_nesLineBuf[3] = BLACK;
 
         for (int32_t x = 4; x < frame_width - 1; x++) {
-            int srcX = (x * NES_SCREEN_WIDTH) / frame_width - 4; // hack to avoid frame bleeding
-            lineBuf[x] = myPalette[data[srcY][srcX]];
+            int srcX = (x * NES_SCREEN_WIDTH) / frame_width - 4;
+            s_nesLineBuf[x] = myPalette[data[srcY][srcX]];
         }
-        M5.Lcd.pushPixels(lineBuf, frame_width);
-        lineBuf[frame_width - 2] = BLACK; // same
-        lineBuf[frame_width - 1] = BLACK;
+
+        s_nesLineBuf[frame_width - 2] = BLACK;
+        s_nesLineBuf[frame_width - 1] = BLACK;
+
+        M5.Lcd.pushPixels(s_nesLineBuf, frame_width);
     }
     M5.Lcd.endWrite();
 }
